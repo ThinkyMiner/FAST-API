1ca498d2212cada1dd05e6c01a4cf89661b160f5
ThinkyMiner <131332283+ThinkyMiner@users.noreply.github.com>
2023-12-08 16:48:58 +0530

    Made a new file that contains all the schemas

--- app/main.py
+++ app/main.py
@@ -4,19 +4,12 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas
 from .database import engine, SessionLocal,get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-class Post(BaseModel):
-    title : str
-    content : str
-    published: bool
-    id : int = 1
 
 while True:
     try:
@@ -34,16 +27,6 @@ async def root():
     return {"message": "World"}
 
 
-
-@app.get("/sqlalchemy")
-def test_posts(db : Session = Depends(get_db)):
-
-    posts = db.query(models.Post).all()
-    print(posts)
-    return {"data": "success"}
-
-
-
 @app.get("/posts")
 def post(db : Session = Depends(get_db)):
     # the common sql code used is in the 2 lines lower
@@ -56,12 +39,12 @@ def post(db : Session = Depends(get_db)):
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)# or to change the status code we can just pass another attribute next to posts as status_code=status.HTTP....
-def create_post(post : Post, db : Session = Depends(get_db)):
+def create_post(post : schemas.CreatePost, db : Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title , post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
     
-    new_post = models.Post(title=post.title, content=post.content, published=post.published)
+    new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -102,7 +85,7 @@ def delete_post(id: int, db : Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post:Post, db : Session = Depends(get_db)):
+def update_post(id: int, post:schemas.CreatePost, db : Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s , content = %s , published = %s WHERE id = %s RETURNING * """ , (post.title , post.content , post.published , str(id)))
     # updated_post = cursor.fetchone()
     # conn.commit()
